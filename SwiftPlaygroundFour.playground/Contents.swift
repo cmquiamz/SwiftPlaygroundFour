33e4017022b28fa28296e82b45b95d7a33e5a7f1
Court Q <cmquiamz@hawaii.edu>
2018-02-20 14:54:22 -1000

    Completed Assignment #6

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -1,9 +1,3 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "Hello, playground"
-
 //for number in 1...5
 //{
     //print(number)
@@ -24,6 +18,23 @@ while i <= 10
     //we want to add +1 to i for each iteration the loop
     i = i + 1
 }
+
+//Coding Challenge #1
+var a = 1
+while a <= 3
+{
+    print(a)
+    a = a + 1
+}
+
+//Coding Challeng #2
+var newSum = 10
+for number in 1...10
+{
+    newSum -= number
+    print(newSum)
+}
+
 
 
 
